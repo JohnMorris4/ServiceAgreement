584a332796c4a43894bf81d21fe00220a8abaee1
johnmorris4 <johnmorris4@bellsouth.net>
2021-03-10 04:01:27 -0500

    Added Scripts

---
+++ Assets/Insurance App/assets/Scripts/Panels/BorderPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderPanel : MonoBehaviour, IPanel
+{
+    public void ProcessInfo()
+    {
+        
+    }
+}
---
+++ Assets/Insurance App/assets/Scripts/Panels/LocationPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationPanel : MonoBehaviour, IPanel
+{
+    public void ProcessInfo()
+    {
+        
+    }
+}
